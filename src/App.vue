ab4ce0cfb378403b61ae3582d75cb5de5764c46a
t22499 <t2249935621@163.com>
2023-07-01 19:28:08 +0800

    scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,14 @@
    <div class="app">
       <!-- 一级组件出口 -->
       <RouterView></RouterView>
+      <div class="text">aaaaaa</div>
    </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.app{
+   .text{
+      color: $priceColor;
+   }
+}
 </style>
