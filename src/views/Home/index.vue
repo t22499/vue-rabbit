829ea57fe992a5b1469bf7dbe1d053807d56d74f
t22499 <t2249935621@163.com>
2023-07-02 13:04:18 +0800

    home面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,8 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+
+import HomePanel from '@/views/Home/components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +16,11 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <!-- //测试 -->
+  <HomePanel title="新鲜好物" subTitle="新鲜好玩">
+    <div>我是新鲜好物的插槽</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" subTitle="好的商品">
+    <div>我是新鲜好物的插槽</div>
+  </HomePanel>
 </template>
