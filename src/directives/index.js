f3f8cbb192e40513aeb936b5ad41433ffba406b7
t22499 <t2249935621@163.com>
2023-07-02 16:37:19 +0800

    图片懒加载优化

---
+++ src/directives/index.js
@@ -0,0 +1,25 @@
+//定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+export const lazyPlugin = {
+  install (app){
+    //懒加载指令逻辑
+    app.directive('img-lazy',{
+      mounted(el,binding){
+        //el绑定的元素
+        //binding指令等号后面的值
+        const {stop} = useIntersectionObserver(
+          el,
+          ([{ isIntersecting }]) => {
+            console.log(isIntersecting)
+            if(isIntersecting){
+              //进入视口区域
+              el.src = binding.value
+              //如果执行了useIntersectionObserver,就停止当前方法避免内存浪费
+              stop()
+            }
+          },
+        )
+      }
+    })
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import { useIntersectionObserver } from '@vueuse/core'
+//引入懒加载指令并注册
+import {lazyPlugin} from "@/directives/index"
 
 //引入初始化样式文件
 import '@/styles/common.scss'
@@ -11,22 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
+//注册懒加载
+app.use(lazyPlugin)
 
-//定义全局指令
-app.directive('img-lazy',{
-  mounted(el,binding){
-    //el绑定的元素
-    //binding指令等号后面的值
-    useIntersectionObserver(
-      el,
-      ([{ isIntersecting }]) => {
-        if(isIntersecting){
-          //进入视口区域
-          el.src = binding.value
-        }
-      },
-    )
-  }
-})
