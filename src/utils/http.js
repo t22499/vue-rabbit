c4408ab2a6c5adf65edc3c308b810f5a5f618376
t22499 <t2249935621@163.com>
2023-07-06 23:50:35 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,12 +3,16 @@ import axios from "axios";
 import 'element-plus/es/components/message/style/css'
 import { ElMessage } from 'element-plus'
 import {useUserStore} from '@/stores/user'
+import router from '@/router/index';
+
  
 const httpInstance = axios.create({
   baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout:5000
 })
  
+
+
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
@@ -23,11 +27,20 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
+  
   //错误提示
   ElMessage({
     type:'warning',
     message:e.response.data.message
   })
+  //401token失效处理
+  //清除本地数据
+  //跳转登录页面
+  if(e.response.status === 401) {
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
