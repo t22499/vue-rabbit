4cfa1cd5314f542fb7c2277e8192c41463200f9d
t22499 <t2249935621@163.com>
2023-07-06 22:11:36 +0800

    设置请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,14 +2,22 @@
 import axios from "axios";
 import 'element-plus/es/components/message/style/css'
 import { ElMessage } from 'element-plus'
-
+import {useUserStore} from '@/stores/user'
+ 
 const httpInstance = axios.create({
   baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout:5000
 })
+ 
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+  //获取token
+  const userStore = useUserStore()
+  const token = userStore.userInfo.token
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e => Promise.reject(e))
 
